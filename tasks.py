bd801e1c299571fac62a6e91b63feac656198875
Martin Virtel <martin.virtel@gmail.com>
2019-08-06 14:06:25 +0200

    title

--- tasks.py
+++ tasks.py
@@ -33,3 +33,17 @@ def dev(c):
 
 
     """)
+
+@task
+def deploy(c):
+    c.run(f"""
+        cd {HERE}
+        export DEPLOY=$(git rev-parse HEAD)
+        npm run build-css && \
+        PUBLIC_URL=/followme/ elm-app build &&\
+        cd build-remote && \
+        cp -rvu ../build/* . && \
+        git add * && \
+        git commit -a -m "built from $DEPLOY"
+        git push
+    """)
